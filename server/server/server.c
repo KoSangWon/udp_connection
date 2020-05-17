d47e71afc7610ade79191534f999f7b5c069d9a2
Sangwon <jackoss@naver.com>
2020-05-18 00:34:55 +0900

    init

---
+++ server/server/server.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  server
+//
+//  Created by 고상원 on 2020/05/17.
+//  Copyright © 2020 고상원. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
